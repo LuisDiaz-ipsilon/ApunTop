9a5cc58f4c7592e3f0916488572d515254d998be
LuisDiaz-ipsilon <99diazluisfernand@gmail.com>
2021-05-06 12:53:34 -0600

    carpetaComponentes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NavbarComponent } from "../app/components/shared/navbar/navbar.component"; 
+
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   constructor() {}
+
+  ngOnInit(){
+    
+  }
 }
