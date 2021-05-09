1deee59561c470f44580ec8809792b4871355011
LuisDiaz-ipsilon <99diazluisfernand@gmail.com>
2021-05-08 23:20:27 -0600

    pathNavBar
    Se agrego el path de navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
 
 const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
-  }
+  },
+  { path: 'navbar', component: NavbarComponent },
 ];
 @NgModule({
   imports: [
