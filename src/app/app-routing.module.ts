2744ffd3e7e17b059b5d235bffcf5f62afd98e1c
LuisDiaz-ipsilon <99diazluisfernand@gmail.com>
2021-05-08 23:52:59 -0600

    DELTabs

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
 
 const routes: Routes = [
-  {
-    path: '',
-    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
-  },
+  { path: '', component: HomeComponent },
   { path: 'navbar', component: NavbarComponent },
 ];
 @NgModule({
