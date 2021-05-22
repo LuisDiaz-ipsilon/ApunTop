4fa95df4a22c66e3195eef4c41640e4a77d03180
LuisDiaz-ipsilon <99diazluisfernand@gmail.com>
2021-05-21 21:10:23 -0600

    SolPositionBtnSalir

--- src/app/components/shared/navbar/navbar.component.ts
+++ src/app/components/shared/navbar/navbar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { LoginService } from '../../../login/login.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -7,10 +9,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  constructor(private loginService: LoginService, private router: Router ) { }
 
   ngOnInit() {}
 
-  
+  onLogout() {
+    this.loginService.logout();
+    this.router.navigateByUrl('login');
+  }
 
 }
