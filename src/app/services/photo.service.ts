0f06d0b236ccdb569f9305a20e6c22c4bd616de0
LuisDiaz-ipsilon <99diazluisfernand@gmail.com>
2021-05-26 11:41:21 -0600

    camera-with-fn.Save

--- src/app/services/photo.service.ts
+++ src/app/services/photo.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Camera, CameraPhoto, CameraResultType, CameraSource, Photo } from '@capacitor/camera';
 import { Filesystem, Directory } from '@capacitor/filesystem';
 import { Storage } from '@capacitor/storage';
+import { Base64, } from '@ionic-native/base64/ngx';
 
 
 @Injectable({
@@ -10,13 +11,30 @@ import { Storage } from '@capacitor/storage';
 export class PhotoService {
 
   photos;
+
+  constructor(private base64: Base64) { }
+
+  private async readAsBase64(cameraPhoto: Photo) {
+    // Fetch the photo, read as a blob, then convert to base64 format
+    const response = await fetch(cameraPhoto.webPath!);
+    const blob = await response.blob();
+
+    return await this.convertBlobToBase64(blob) as string;
+  }
+
+
+  convertBlobToBase64 = (blob: Blob) => new Promise((resolve, reject) => {
+    const reader = new FileReader;
+    reader.onerror = reject;
+    reader.onload = () => {
+      resolve(reader.result);
+    };
+    reader.readAsDataURL(blob);
+  });
   
-
-  constructor() { }
-
   private async savePicture(cameraPhoto: Photo) {
     // Convert photo to base64 format, required by Filesystem API to save
-    /*const base64Data = await this.readAsBase64(cameraPhoto);
+    const base64Data = await this.readAsBase64(cameraPhoto);
 
     // Write the file to the data directory
     const fileName = new Date().getTime() + '.jpeg';
@@ -31,7 +49,7 @@ export class PhotoService {
     return {
       filepath: fileName,
       webviewPath: cameraPhoto.webPath
-    };*/
+    };
   }
 
   public async addNewToGallery() {
